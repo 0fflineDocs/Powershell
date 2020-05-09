ccb11f6333afd5a18178ef26aea42c184827dc6d
0fflineDocs <simon.ludvig.hakansson@outlook.com>
2020-05-09 18:18:06 +0200

    Create ControlledFolderAccess.ps1

---
+++ Windows/Security/Checks/ControlledFolderAccess.ps1
@@ -0,0 +1,25 @@
+if ($ControlledFolderAccess) {
+    $p = Get-MpPreference -ErrorAction SilentlyContinue
+    $c = Get-MpComputerStatus -ErrorAction SilentlyContinue
+    $p = @($p)
+    $CFAstatus = ($p += $c)
+    try {
+        Write-LogEntry -Message "Checking status and configuration for Attack Surface Reduction..."
+        if ($CFAstatus.EnableControlledFolderAccess -eq "2") 
+    {
+    Write-LogEntry -Message "Controlled Folder Access is enabled and in audit mode."
+    }
+    elseif ($CFAstatus.EnableControlledFolderAccess -eq "1") 
+    {
+        Write-LogEntry -Message "Controlled Folder Access is configured and enforced."
+    }
+        else 
+        {
+            Write-LogEntry -Message "Controlled Folder Access is not configured."
+        }
+}
+catch [System.Exception] 
+    {
+        Write-LogEntry -Message "Failed to check status of $ControlledFolderAccess"
+    }
+}
