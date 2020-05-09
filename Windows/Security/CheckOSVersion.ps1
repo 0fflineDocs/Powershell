c9d495af78d3d5dbb9428267c9d72e60bd08e70a
0fflineDocs <simon.ludvig.hakansson@outlook.com>
2020-05-09 18:14:01 +0200

    Update CheckOSVersion.ps1

--- Windows/Security/CheckOSVersion.ps1
+++ Windows/Security/CheckOSVersion.ps1
@@ -1,12 +1,12 @@
-#Variables
-$win32os = Get-CimInstance Win32_OperatingSystem -computername $PC | select Name, OSArchitecture, Version, Buildnumber, OperatingSystemSKU -ErrorAction silentlycontinue
-$WindowsEdition = $win32os.Name
-$OSArchitecture = $win32os.OSArchitecture 
-$WindowsBuild = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"-ErrorAction silentlycontinue).ReleaseId 
-$BuildNumber = $win32os.Buildnumber
-
-#Products
-Clients OS-Edition is: $WindowsEdition
-Clients OS-Architecture is: $OSArchitecture
-Clients OS-Version is: $WindowsBuild
-Clients OS-Buildnumber is: $BuildNumber
+if($OS){
+    Write-LogEntry -Message "[Operating System]"
+    $win32os = Get-CimInstance Win32_OperatingSystem -computername $PC | select Name, OSArchitecture, Version, Buildnumber, OperatingSystemSKU -ErrorAction silentlycontinue
+    $WindowsEdition = $win32os.Name
+    $OSArchitecture = $win32os.OSArchitecture 
+    $WindowsBuild = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"-ErrorAction silentlycontinue).ReleaseId 
+    $BuildNumber = $win32os.Buildnumber
+    Write-LogEntry -Message "Clients OS-Edition is: $WindowsEdition"
+    Write-LogEntry -Message "Clients OS-Architecture is: $OSArchitecture"
+    Write-LogEntry -Message "Clients OS-Version is: $WindowsBuild"
+    Write-LogEntry -Message "Clients OS-Buildnumber is: $BuildNumber"
+}
