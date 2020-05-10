de43283ee3c43b991bb996541c62e92c29dafd24
0fflineDocs <simon.ludvig.hakansson@outlook.com>
2020-05-10 18:11:30 +0200

    Update ApplicationGuard.ps1

--- Windows/Security/Checks/ApplicationGuard.ps1
+++ Windows/Security/Checks/ApplicationGuard.ps1
@@ -1,19 +1,35 @@
-if($ApplicationGuard){
-    try {
+Function Get-ApplicationGuard(){
+    <#
+    .DESCRIPTION
+    Checks Application Guard-status
+        
+    .EXAMPLE
+    Get-ApplicationGuard
+    #>
+    
+    #Variable
     Write-LogEntry -Message "[Application Guard]"
     Write-LogEntry -Message "Checking if Windows Defender Application Guard is installed and enabled..." 
     $ApplicationGuardStatus = Get-WindowsOptionalFeature -Online -Featurename Windows-Defender-ApplicationGuard -ErrorAction SilentlyContinue
-    if ($ApplicationGuardStatus.State = "Enabled") 
-    {
-    Write-Logentry -Message "Windows Defender Application Guard is installed and enabled."
-    }
-    else 
-            {
-                Write-LogEntry -Message "Windows Defender Application Guard is not enabled..."
-            }
-    }
-            catch [System.Exception] 
-            {
-                Write-LogEntry -Message "Failed to check status of $ApplicationGuard"
-            }
+
+    try {
+   
+        if ($ApplicationGuardStatus.State = "Enabled") 
+        {
+        Write-Logentry -Message "Windows Defender Application Guard is installed and enabled."
         }
+        else 
+                {
+                    Write-LogEntry -Message "Windows Defender Application Guard is not enabled..."
+                }
+        }
+                catch [System.Exception] 
+                {
+                    Write-LogEntry -Message "Failed to check status of $ApplicationGuard"
+                }
+            
+            catch {
+                Write-Error $_.Exception 
+                break
+            }
+}
