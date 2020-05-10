ccee319b0f2ffd82ddac1303af623712f2e86514
0fflineDocs <simon.ludvig.hakansson@outlook.com>
2020-05-10 18:20:49 +0200

    Update and rename Windows/Security/Checks/CredentialGuard.ps1 to Windows/Security/Functions/CredentialGuard.ps1

---
+++ Windows/Security/Functions/CredentialGuard.ps1
@@ -0,0 +1,39 @@
+Function Get-CredentialGuard(){
+        <#
+        .DESCRIPTION
+        Checks status of Credential Guard
+                
+        .EXAMPLE
+        Get-CredentialGuard
+        #>
+        
+        #Variable
+        $CredentialguardStatus = Get-CimInstance -ClassName Win32_DeviceGuard -Namespace root\Microsoft\Windows\DeviceGuard -ErrorAction SilentlyContinue
+            try {
+                Write-LogEntry -Message "[Credential Guard]" 
+                if ($CredentialGuardStatus.SecurityServicesRunning -like 1)
+            {
+                Write-Logentry -Message "Credential Guard Services are running."
+            }
+            else 
+                    {
+                        Write-LogEntry -Message "CredentialGuard Service are not running."
+                    }
+                    if ($CredentialGuardStatus.SecurityServicesConfigured -like 1)
+                    {
+                        Write-Logentry -Message "Credential Guard is configured."
+                    }
+                    else 
+                            {
+                                Write-LogEntry -Message "Credential Guard is not configured."
+                            }
+        }
+        catch [System.Exception] 
+                    {
+                        Write-LogEntry -Message "Failed to check status of $CredentialGuard"
+                    }  
+                    catch {
+                        Write-Error $_.Exception 
+                        break
+                    }
+}
