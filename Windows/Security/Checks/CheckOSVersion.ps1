a6b06e227554bf7fb7b903b249e0a41f369c4a31
0fflineDocs <simon.ludvig.hakansson@outlook.com>
2020-05-10 11:57:40 +0200

    Update CheckOSVersion.ps1

--- Windows/Security/Checks/CheckOSVersion.ps1
+++ Windows/Security/Checks/CheckOSVersion.ps1
@@ -1,12 +1,28 @@
-if($OS){
-    Write-LogEntry -Message "[Operating System]"
-    $win32os = Get-CimInstance Win32_OperatingSystem -computername $PC | select Name, OSArchitecture, Version, Buildnumber, OperatingSystemSKU -ErrorAction silentlycontinue
-    $WindowsEdition = $win32os.Name
-    $OSArchitecture = $win32os.OSArchitecture 
-    $WindowsBuild = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"-ErrorAction silentlycontinue).ReleaseId 
-    $BuildNumber = $win32os.Buildnumber
-    Write-LogEntry -Message "Clients OS-Edition is: $WindowsEdition"
-    Write-LogEntry -Message "Clients OS-Architecture is: $OSArchitecture"
-    Write-LogEntry -Message "Clients OS-Version is: $WindowsBuild"
-    Write-LogEntry -Message "Clients OS-Buildnumber is: $BuildNumber"
+Function Get-DeviceOperatingSystem(){
+    <#
+    .DESCRIPTION
+    Checks the current PCs Operating System Edition, Architecture, Version, and Build.
+    
+    .EXAMPLE
+    Get-DeviceOperatingSystem
+    #>
+
+    #Variable
+    $win32os = Get-CimInstance Win32_OperatingSystem -computername $PC | Select-Object Name, OSArchitecture, Version, Buildnumber, OperatingSystemSKU -ErrorAction silentlycontinue
+        try {
+            Write-LogEntry -Message "[Operating System]"
+            $win32os = Get-CimInstance Win32_OperatingSystem -computername $PC | Select-Object Name, OSArchitecture, Version, Buildnumber, OperatingSystemSKU -ErrorAction silentlycontinue
+            $WindowsEdition = $win32os.Name
+            $OSArchitecture = $win32os.OSArchitecture 
+            $WindowsBuild = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"-ErrorAction silentlycontinue).ReleaseId 
+            $BuildNumber = $win32os.Buildnumber
+            Write-LogEntry -Message "Clients OS-Edition is: $WindowsEdition"
+            Write-LogEntry -Message "Clients OS-Architecture is: $OSArchitecture"
+            Write-LogEntry -Message "Clients OS-Version is: $WindowsBuild"
+            Write-LogEntry -Message "Clients OS-Buildnumber is: $BuildNumber"
+            }
+        catch {
+            Write-Error $_.Exception 
+            break
+        }
 }
