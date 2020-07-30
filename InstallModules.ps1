2ca1afddbd8ab5cd0c21798308f3d9c3857bcc9f
0fflineDocs <simon.ludvig.hakansson@outlook.com>
2020-07-30 10:02:50 +0200

    Update InstallModules.ps1

--- InstallModules.ps1
+++ InstallModules.ps1
@@ -1,13 +1,10 @@
 #Modules
 Install-Module PowerShellGet -force
 Install-Module ActiveDirectory -force
+Install-WindowsFeature RSAT-AD-PowerShell
 Install-Module AzureAD -force
 Install-Module Microsoft.Graph.Intune -force
 Install-Module WindowsAutopilotIntune -force
 Install-Module -Name IntuneWin32App
 Install-module -Name Exchangeonline -Force
-Install-Module -Name ExchangeOnlineManagement -Repository PSGallery -Scope AllUsers -MinimumVersion 0.3374.9 -AllowClobber -Confirm:$false -Force -AcceptLicense
-
-#Scripts
-Install-Script AutopilotDeviceSync
-Install-Script -Name Get-LatestAdobeReaderInstaller
+Install-Module -Name ExchangeOnlineManagement -Repository PSGallery -Scope AllUsers -MinimumVersion 0.3374.9 -AllowClobber -Confirm:$false -Force
