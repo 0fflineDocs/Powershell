6dc359963e5bc1c028d6ebf39e81c815eea61831
0fflineDocs <simon.ludvig.hakansson@outlook.com>
2021-03-22 09:30:02 +0100

    Update InstallModules.ps1

--- InstallModules.ps1
+++ InstallModules.ps1
@@ -1,10 +1,16 @@
 #Modules
 Install-Module PowerShellGet -force
+Set-ExecutionPolicy Bypass
+Import-Module PowerShellGet
+
 Install-Module ActiveDirectory -force
-Install-WindowsFeature RSAT-AD-PowerShell
+Enable-WindowsOptionalFeature -Online -FeatureName RSATClient-Roles-AD-Powershell (CLIENTOS)
+Install-WindowsFeature RSAT-AD-PowerShell (SERVEROS)
 Install-Module AzureAD -force
+Install-Module AzureADPreview -AllowClobber -force
 Install-Module Microsoft.Graph.Intune -force
 Install-Module WindowsAutopilotIntune -force
 Install-Module -Name IntuneWin32App
 Install-module -Name Exchangeonline -Force
 Install-Module -Name ExchangeOnlineManagement -Repository PSGallery -Scope AllUsers -MinimumVersion 0.3374.9 -AllowClobber -Confirm:$false -Force
+Install-Module PSReadline -AllowPrerelease -force
