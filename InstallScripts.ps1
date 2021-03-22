43d65def947476160c7b76f179464581231374c4
0fflineDocs <simon.ludvig.hakansson@outlook.com>
2021-03-22 09:30:39 +0100

    Update InstallScripts.ps1

--- InstallScripts.ps1
+++ InstallScripts.ps1
@@ -1,4 +1,10 @@
 #Install Scripts
-Install-Script -Name AutopilotESPStatus
-Install-Script AutopilotDeviceSync
+Install-Script -Name AutopilotHealthCheck 
+Install-Script -Name Get-AutopilotESPStatus
+Install-Script -Name Get-AutopilotDeviceSync
 Install-Script -Name Get-LatestAdobeReaderInstaller
+Install-Script -Name Convert-WindowsAutopilotProfile 
+Install-Script -Name Get-CMAutopilotHashes
+Install-Script -NAme Get-AutopilotDiagnostics.ps1
+Install-Script -Name Get-LatestAdobeReaderInstaller
+Install-Script -Name SecurityPosture
